e34987c5f70bee0fe07bef29c7fb9766ef0c650d
Connor Sears <connordsears@gmail.com>
2014-06-03 20:12:15 -0500

    working buttons n sounds

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 // JavaScript Document gooooooo
 $(document).ready(function(){
 	
-	//var winHeight = $(window).height();
-	//$('.splash').css("height", winHeight+'px');
+	var tubeAudio = document.createElement('audio');
+        tubeAudio.setAttribute('src', 'sounds/rock-tube.mp3');
+        //tubeAudio.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+
+        $.get();
+
+        tubeAudio.addEventListener("load", function() {
+            tubeAudio.play();
+        }, true);
+
+        $('#tbut').click(function() {
+            ssAudio.pause();
+			tubeAudio.play();
+        });
+		
+	var ssAudio = document.createElement('audio');
+        ssAudio.setAttribute('src', 'sounds/rock-ss.mp3');
+        //tubeAudio.setAttribute('autoplay', 'autoplay');
+        //audioElement.load()
+
+        $.get();
+
+        ssAudio.addEventListener("load", function() {
+            ssAudio.play();
+        }, true);
+
+        $('#ssbut').click(function() {
+            tubeAudio.pause();
+			ssAudio.play();
+        });
+
+       // $('.pause').click(function() {
+         //   audioElement.pause();
+        //});
 	
 });
