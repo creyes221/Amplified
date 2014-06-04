c6b1279a3b08d0b2f05d5214d8da0f340c0ab743
creyes221 <carlosreyes2015@u.northwestern.edu>
2014-06-03 21:51:01 -0500

    Updated JS to use Howler and use state vars

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 // JavaScript Document gooooooo
 $(document).ready(function(){
+		  
+	var ssound = new Howl({
+	    urls: ['sounds/rock-tube.mp3']
+	  })
+	  
+	var tsound = new Howl({
+	    urls: ['sounds/rock-ss.mp3']
+	  })
+	  
+	  var sstate = 0;
+	  var tstate = 0;
+	  var tpos, spos;
+	  
+	$('#tbut').click(function() {
+	  tpos = ssound.pos();
+	  
+	  if(tstate == 0 | tpos == 0){
+	    tsound.pause();
+      ssound.play();
+      tstate = 1
+      sstate = 0
+      /*if(pos == 24){
+        tstate = 0
+      }*/
+      //24.5
+	  }
+	  
+	  else if(tstate == 1){
+	    ssound.pause();
+	    tstate = 0
+	  }
+	  
+	})
 	
-	var tubeAudio = document.createElement('audio');
-        tubeAudio.setAttribute('src', 'sounds/rock-tube.mp3');
-        //tubeAudio.setAttribute('autoplay', 'autoplay');
-        //audioElement.load()
-
-        $.get();
-
-        tubeAudio.addEventListener("load", function() {
-            tubeAudio.play();
-        }, true);
-
-        $('#tbut').click(function() {
-            ssAudio.pause();
-			tubeAudio.play();
-        });
-		
-	var ssAudio = document.createElement('audio');
-        ssAudio.setAttribute('src', 'sounds/rock-ss.mp3');
-        //tubeAudio.setAttribute('autoplay', 'autoplay');
-        //audioElement.load()
-
-        $.get();
-
-        ssAudio.addEventListener("load", function() {
-            ssAudio.play();
-        }, true);
-
-        $('#ssbut').click(function() {
-            tubeAudio.pause();
-			ssAudio.play();
-        });
-
-       // $('.pause').click(function() {
-         //   audioElement.pause();
-        //});
+	$('#ssbut').click(function() {
+	  spos = tsound.pos();
+	  if(sstate == 0 | spos == 0){
+	    ssound.pause();
+      tsound.play();
+      sstate = 1
+      tstate = 0
+	  }
+	  
+	  else if(sstate == 1){
+	    tsound.pause();
+	    sstate = 0
+	  }
+	  
+	})
+	//var winHeight = $(window).height();
+	//$('.splash').css("height", winHeight+'px');
 	
 });
